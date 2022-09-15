097d5fdeb8200b74853d99132de21a22b40d7847
kcamozzi <kevincamozzi@gmail.com>
2022-09-15 15:41:24 -0700

    Frontend retrieve tweet successful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,24 @@ function App() {
 
  
   /*retreive from backend*/
-   
-  /*  
+    
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
-    fetch("/").then(
+    fetch("/retrieve?tweetId=1570534434428026880").then(
       response => response.json()
     ).then(
       data => {
         setBackendData(data)
       }
     )
-  }, []) */
+  }, []) 
 
   return (
     <div className="App">
       <header className="App-header">
       <h3>Twitter Lab</h3>
+      <p>{backendData.message}</p>
       </header>
     </div>
   );
