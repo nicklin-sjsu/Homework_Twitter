5ca66b7d48adf97af894754ac7445c4b585ad7d7
Bryan Caldera <bryanicaldera@gmail.com>
2022-09-16 14:28:30 -0700

    create tweet works, also backend port is 5001 now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,80 @@
 //import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react'
+const axios = require('axios');
 
 function App() {
 
- 
+
   /*retreive from backend*/
-    
-  const [backendData, setBackendData] = useState([{}])
+
+  const [newTweet, setNewTweet] = useState("")
 
   useEffect(() => {
-    fetch("/retrieve?tweetId=1570534434428026880").then(
+    /*fetch("/retrieve?tweetId=1570534434428026880").then(
       response => response.json()
     ).then(
       data => {
         setBackendData(data)
       }
     )
-  }, []) 
+    <p>{backendData.message}</p>
+    */
+  }, [])
+
+  const handleCreateTweet = async () => {
+    await axios.get('/create', {
+      params: {message: newTweet},
+    })
+      .then(function (response) {
+        console.log(response);
+        setNewTweet("");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const handleDeleteTweet = () => {
+
+  }
+
+  const handleRetrieveTweet = () => {
+
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <h3>Twitter Lab</h3>
-      <p>{backendData.message}</p>
-      </header>
+    <div className="App container-fluid text-center bg-dark vh-100">
+
+      <br />
+      <div className="text-light">
+        <h1>Twitter Lab</h1>
+      </div>
+      <br />
+
+      <div>
+        <form className='text-center text-light container border border-1 border-secondary rounded-3' onSubmit={() => handleCreateTweet()}>
+          
+          <br />
+          <div className='row'>
+            <h3><label>Create a tweet</label></h3>
+          </div>
+
+          <div className='row justify-content-center'>
+            <input className='col-8 rounded-3' type="text" placeholder="tweet" value={newTweet} onChange={(e) => setNewTweet(e.target.value)} />
+          </div>
+          <br />
+
+          <button className="row btn btn-primary" type="submit">
+            Submit
+          </button>
+          <br />
+          <br />
+          
+        </form>
+      </div>
+
     </div>
   );
 }
